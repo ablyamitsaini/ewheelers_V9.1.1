2055d5c1f9cfffe57bc8b433d2fd23b891cadf11
Anup Rawat <anuprawat@4demo.biz>
2018-04-24 13:04:03 +0530

    DB conf update

--- public/settings.php
+++ public/settings.php
@@ -5,15 +5,15 @@ define('CONF_WEBROOT_BACKEND', '/admin/');
 if(file_exists(dirname(__DIR__) . '/conf/'.$_SERVER['SERVER_NAME'].'.php')){
 	require_once(dirname(__DIR__) . '/conf/'.$_SERVER['SERVER_NAME'].'.php');
 }else{
+/* 	define('CONF_DB_SERVER', 'localhost');
+	define('CONF_DB_USER', 'staging');
+	define('CONF_DB_PASS', 'staging');
+	define('CONF_DB_NAME', 'staging_yokart_v8_1_test'); */
+	
+	//die('Domain specific settings file missing');
 	define('CONF_DB_SERVER', 'localhost');
 	define('CONF_DB_USER', 'staging');
 	define('CONF_DB_PASS', 'staging');
-	define('CONF_DB_NAME', 'staging_yokart_v8_1_test');
-	
-	//die('Domain specific settings file missing');
-	/* define('CONF_DB_SERVER', 'localhost');
-	define('CONF_DB_USER', 'staging');
-	define('CONF_DB_PASS', 'staging');
-	define('CONF_DB_NAME', 'staging_yokart_v8_1'); */
+	define('CONF_DB_NAME', 'staging_yokart_v8_1');
 }
 ?>
