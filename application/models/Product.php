dba38c31d1eb10becc9774efcd14437e8f7a3fa6
Manpreet Kaur <manpreetkaur@4demo-bestech.router0481c4.com>
2018-10-18 14:49:58 +0530

    task-54743-Fixed engagespot issue

--- application/models/Product.php
+++ application/models/Product.php
@@ -1163,8 +1163,7 @@ class Product extends MyAppModel{
 		if(!in_array($sortOrder,array('asc','desc'))){
 			$sortOrder = 'asc';
 		}		
-				
-				
+
 		$srch->setPageNumber($page);		
 		if( $pageSize ){
 			$srch->setPageSize($pageSize);
