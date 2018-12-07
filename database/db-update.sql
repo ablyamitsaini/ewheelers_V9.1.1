7fa5153e1c28ea2a6f489425d0cd75afeef57957
Anup Rawat <anuprawat@4demo-bestech.router0481c4.com>
2018-12-07 18:02:33 +0530

    bug-022348 updated table name

--- database/db-update.sql
+++ database/db-update.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `tbl_languages` CHANGE `language_code` `language_code` VARCHAR(5) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
 
-CREATE TABLE `tbl_order_coupons_hold` (
+CREATE TABLE `tbl_coupons_hold_pending_order` (
   `ochold_order_id` varchar(15) NOT NULL,
   `ochold_coupon_id` int(11) NOT NULL,
   `ochold_added_on` datetime NOT NULL
@@ -11,7 +11,7 @@ CREATE TABLE `tbl_order_coupons_hold` (
 --
 
 --
--- Indexes for table `tbl_order_coupons_hold`
+-- Indexes for table `tbl_coupons_hold_pending_order`
 --
-ALTER TABLE `tbl_order_coupons_hold`
+ALTER TABLE `tbl_coupons_hold_pending_order`
   ADD PRIMARY KEY (`ochold_order_id`,`ochold_coupon_id`);
