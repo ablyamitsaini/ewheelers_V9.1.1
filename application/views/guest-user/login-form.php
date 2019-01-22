bd355e2c0fbf7f0582ca603ae6c43d8f9a20503b
Pawan Yadav Dz <dzpawan@4demo.biz>
2019-01-22 15:11:56 +0530

    task-45836 fixed some inner pages CSS

--- application/views/guest-user/login-form.php
+++ application/views/guest-user/login-form.php
@@ -1,17 +1,19 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.'); ?>
 
 <div id="body" class="body bg--gray">
-  <section class="top-space ">
+  <section class="section">
     <div class="container">
-      <div class="panel panel--centered">
-        <div class="box box--white box--tabled">
-          <div class="box__cell <?php echo (empty($pageData)) ? 'noborder--right noborder--left' : '';?>">
-            <?php $this->includeTemplate('guest-user/loginPageTemplate.php', $data,false ); ?>
+
+        <div class="row justify-content-center">
+          <div class="col-md-6 <?php echo (empty($pageData)) ? '' : '';?>">
+            <div class="box box--white box--space">
+              <?php $this->includeTemplate('guest-user/loginPageTemplate.php', $data,false ); ?>
+            </div>
           </div>
           <?php if(!empty($pageData)) { $this->includeTemplate('_partial/GuestUserRightPanel.php', $pageData ,false ); } ?>
         </div>
-      </div>
+
     </div>
-    <div class="gap"></div>
+
   </section>
 </div>
