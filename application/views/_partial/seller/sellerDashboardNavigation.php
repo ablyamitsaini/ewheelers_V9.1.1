c8c756113e2a6158e0b2906b32566c259c9eee37
Pawan Yadav Dz <dzpawan@4demo.biz>
2019-02-22 14:58:27 +0530

    task-45836-worked on UI distortion

--- application/views/_partial/seller/sellerDashboardNavigation.php
+++ application/views/_partial/seller/sellerDashboardNavigation.php
@@ -11,7 +11,7 @@ $action = strtolower($action);
 			$logoUrl = CommonHelper::generateUrl();
 		}
 		?>
-		<div class="logo-dashboard"><a href="<?php echo $logoUrl; ?>"><img src="<?php echo CommonHelper::generateFullUrl('Image','siteLogo',array($siteLangId), CONF_WEBROOT_FRONT_URL); ?>" alt="<?php echo FatApp::getConfig('CONF_WEBSITE_NAME_'.$siteLangId) ?>" title="<?php echo FatApp::getConfig('CONF_WEBSITE_NAME_'.$siteLangId) ?>"></a></div>
+		<div class="logo-dashboard"><img src="<?php echo CommonHelper::generateFullUrl('Image','siteLogo',array($siteLangId), CONF_WEBROOT_FRONT_URL); ?>" alt="<?php echo FatApp::getConfig('CONF_WEBSITE_NAME_'.$siteLangId) ?>" title="<?php echo FatApp::getConfig('CONF_WEBSITE_NAME_'.$siteLangId) ?>"></div>
 		<div class="js-hamburger hamburger-toggle"><span class="bar-top"></span><span class="bar-mid"></span><span class="bar-bot"></span></div>
 	</div>
 	<div class="sidebar__content custom-scrollbar">
@@ -37,8 +37,8 @@ $action = strtolower($action);
 				<?php }?>
 				<li class="divider"></li>
 			   <?php }?>
-			   
-			   
+
+
 				<li class="menu__item">
 					<div class="menu__item__inner"> <span class="menu-head"><?php echo Labels::getLabel('LBL_Sales',$siteLangId);?></span></div>
 				</li>
@@ -52,8 +52,8 @@ $action = strtolower($action);
 				<i class="icn shop"><svg class="svg"><use xlink:href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite.svg#manage-shop" href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite.svg#manage-shop"></use></svg>
 				</i><span class="menu-item__title"><?php echo Labels::getLabel("LBL_Order_Return_Requests",$siteLangId); ?></span></a></div></li>
 				<li class="divider"></li>
-				
-				
+
+
 				<li class="menu__item">
 					<div class="menu__item__inner"> <span class="menu-head"><?php echo Labels::getLabel('LBL_Settings',$siteLangId);?></span></div>
 				</li>
@@ -64,7 +64,7 @@ $action = strtolower($action);
 				<i class="icn shop"><svg class="svg"><use xlink:href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite.svg#manage-shop" href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite.svg#manage-shop"></use></svg>
 				</i><span class="menu-item__title"><?php echo Labels::getLabel('LBL_Options',$siteLangId);?></span></a></div></li>
 				<li class="divider"></li>
-				
+
 				<?php if(FatApp::getConfig('CONF_ENABLE_SELLER_SUBSCRIPTION_MODULE')){ ?>
 				<li class="menu__item">
 					<div class="menu__item__inner"> <span class="menu-head"><?php echo Labels::getLabel('LBL_Settings',$siteLangId);?></span></div>
@@ -77,8 +77,8 @@ $action = strtolower($action);
 				</i><span class="menu-item__title"><?php echo Labels::getLabel('LBL_Subscription_Packages',$siteLangId);?></span></a></div></li>
 				<li class="divider"></li>
 				<?php } ?>
-				
-				
+
+
 				<li class="menu__item">
 					<div class="menu__item__inner"> <span class="menu-head"><?php echo Labels::getLabel("LBL_Profile",$siteLangId); ?></span></div>
 				</li>
@@ -98,8 +98,8 @@ $action = strtolower($action);
 				<i class="icn shop"><svg class="svg"><use xlink:href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite.svg#manage-shop" href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite.svg#manage-shop"></use></svg>
 				</i><span class="menu-item__title"><?php echo Labels::getLabel('LBL_Change_Email',$siteLangId);?></span></a></div></li>
 				<li class="divider"></li>
-				
-				
+
+
 				<li class="menu__item">
 					<div class="menu__item__inner"> <span class="menu-head"><?php echo Labels::getLabel("LBL_Reports",$siteLangId); ?></span></div>
 				</li>
@@ -116,7 +116,7 @@ $action = strtolower($action);
 				<i class="icn shop"><svg class="svg"><use xlink:href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite.svg#manage-shop" href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite.svg#manage-shop"></use></svg>
 				</i><span class="menu-item__title"><?php echo Labels::getLabel('LBL_Products_Inventory_Stock_Status', $siteLangId); ?></span></a></div></li>
 				<li class="divider"></li>
-				
+
 			</ul>
 		</nav>
 	</div>
