9f3f883058d0487a75ba3f05a2eff1804d72ec98
Anup Rawat <anuprawat@4demo.biz>
2019-04-06 15:42:54 +0530

    beautified admin files

--- admin-application/controllers/SystemRestoreController.php
+++ admin-application/controllers/SystemRestoreController.php
@@ -1,19 +1,20 @@
 <?php
-class SystemRestoreController extends AdminBaseController {
-	
-	public function __construct($action){
-		parent::__construct($action);
-		$this->admin_id = AdminAuthentication::getLoggedAdminId();		
-	}
-	
-	function index()
+class SystemRestoreController extends AdminBaseController
+{
+    
+    public function __construct($action)
+    {
+        parent::__construct($action);
+        $this->admin_id = AdminAuthentication::getLoggedAdminId();        
+    }
+    function index()
     {
         if (!AdminPrivilege::isAdminSuperAdmin($this->admin_id)) {
-           FatUtility::dieWithError($this->str_invalid_request);
+            FatUtility::dieWithError($this->str_invalid_request);
         }
         $settingsObj = new Settings();
-		
-		$restore_point_frm = $this->getRestorePointForm();
+        
+        $restore_point_frm = $this->getRestorePointForm();
         $post = FatApp::getPostedData();
         if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($post['submit_restore_point'])) {
             if ($settingsObj->compress(CONF_INSTALLATION_PATH . "restore/", CONF_INSTALLATION_PATH . "restore-backups/")) {
@@ -29,35 +30,36 @@ class SystemRestoreController extends AdminBaseController {
         $this->set('restore_point_frm', $restore_point_frm);
         $this->_template->render();
     }
-	
-	function updateSetting($val)
+    
+    function updateSetting($val)
     {
-        $record = new Configurations();	
+        $record = new Configurations();    
         if (!$record->update(array("CONF_AUTO_RESTORE_ON" => $val))) {
-			Message::addErrorMessage($record->getError());
-			FatUtility::dieJsonError( Message::getHtml() );			
-		}
-		$this->set('msg', 'Setting Updated Successfully!!');
-		$this->_template->render(false, false, 'json-success.php');
+            Message::addErrorMessage($record->getError());
+            FatUtility::dieJsonError(Message::getHtml());            
+        }
+        $this->set('msg', 'Setting Updated Successfully!!');
+        $this->_template->render(false, false, 'json-success.php');
     }
 
     protected function getRestorePointForm()
     {
-        $frm = new Form('frmdatabaseBackup',array('id'=>'frmdatabaseBackup'));
+        $frm = new Form('frmdatabaseBackup', array('id'=>'frmdatabaseBackup'));
         $frm->setJsErrorDisplay('afterfield');
         $fld = $frm->addSubmitButton('', 'submit_restore_point', 'Create Restore Point');
         $fld->htmlAfterField = '<small><strong>Notes</strong>: On clicking the above button, system restore point will change to current database & uploads folder and current restore folder will be moved to backup folder with current date attached to it.</small>';
-		$status = 0;
-		$active = "active";
-		if( !FatApp::getConfig('CONF_AUTO_RESTORE_ON',FatUtility::VAR_INT,1) ) {
-			$active = '';
-			$status = 1;
-		}
-        $frm->addHtml('', 'auto_restore', '<div class="field-set"><div class="caption-wraper"><label class="field_label"><strong>Auto Restore</strong></label></div><div class="field-wraper"><div class="field_cover"><label class="statustab '.$active.'" onclick="toggleStatus(event,this)" id="'.$status.'">
+        $status = 0;
+        $active = "active";
+        if(!FatApp::getConfig('CONF_AUTO_RESTORE_ON', FatUtility::VAR_INT, 1) ) {
+            $active = '';
+            $status = 1;
+        }
+        $frm->addHtml(
+            '', 'auto_restore', '<div class="field-set"><div class="caption-wraper"><label class="field_label"><strong>Auto Restore</strong></label></div><div class="field-wraper"><div class="field_cover"><label class="statustab '.$active.'" onclick="toggleStatus(event,this)" id="'.$status.'">
 		  <span data-off="Active" data-on="Inactive" class="switch-labels"></span>
 		  <span class="switch-handles"></span>
-		</label></div></div></div>');
+		</label></div></div></div>'
+        );
         return $frm;
     }
-	
 }
--- admin-application/models/AdminPermissions.php
+++ admin-application/models/AdminPermissions.php
@@ -1,16 +1,19 @@
 <?php
-class AdminPermissions extends MyAppModel{
+class AdminPermissions extends MyAppModel
+{
 
-	const DB_TBL = 'tbl_admin_permissions';
-	const DB_TBL_PREFIX = 'admperm_';
+    const DB_TBL = 'tbl_admin_permissions';
+    const DB_TBL_PREFIX = 'admperm_';
 
-	public function __construct($adminId = 0) {
-		parent::__construct ( static::DB_TBL, static::DB_TBL_PREFIX . 'id', $adminId );
-		$this->objMainTableRecord->setSensitiveFields ( array () );
-	}
-	
-	public static function getSearchObject() {
-		$srch = new SearchBase(static::DB_TBL);
-		return $srch;
-	}
-}	
+    public function __construct($adminId = 0) 
+    {
+        parent::__construct(static::DB_TBL, static::DB_TBL_PREFIX . 'id', $adminId);
+        $this->objMainTableRecord->setSensitiveFields(array ());
+    }
+    
+    public static function getSearchObject() 
+    {
+        $srch = new SearchBase(static::DB_TBL);
+        return $srch;
+    }
+}    
