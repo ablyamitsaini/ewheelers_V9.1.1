d2822f5505090ebced66ffad68737ad408f88cc5
Satbir <satbir.kaushik@fatbit.in>
2019-05-16 15:50:49 +0530

    bug-026717-Duplicate and wrong error message on Price field in setup inventory form.

--- application/controllers/SellerController.php
+++ application/controllers/SellerController.php
@@ -3813,8 +3813,9 @@ class SellerController extends SellerBaseController
         $fld = $frm->addFloatField(Labels::getLabel('LBL_Price', $this->siteLangId).' ['.CommonHelper::getCurrencySymbol(true).']', 'selprod_price');
         $fld->requirements()->setPositive();
         if (isset($productData['product_min_selling_price'])) {
-            $fld->requirements()->setRange($productData['product_min_selling_price'], 9999999999);
-            $fld->requirements()->setCustomErrorMessage(Labels::getLabel('LBL_Minimum_selling_price_for_this_product_is', $this->siteLangId).' '.CommonHelper::displayMoneyFormat($productData['product_min_selling_price'], true, true));
+            $fld->requirements()->setRange(CommonHelper::displayMoneyFormat($productData['product_min_selling_price'], true, true), CommonHelper::displayMoneyFormat(9999999999, true, true));
+            // $fld->requirements()->setCustomErrorMessage(Labels::getLabel('LBL_Minimum_selling_price_for_this_product_is', $this->siteLangId).' '.CommonHelper::displayMoneyFormat($productData['product_min_selling_price'], true, true));
+
             $fld->htmlAfterField='<small class="text--small">'.Labels::getLabel('LBL_This_price_is_excluding_the_tax_rates', $this->siteLangId).'</small> <br><small class="text--small">'.Labels::getLabel('LBL_Min_Selling_price', $this->siteLangId). CommonHelper::displayMoneyFormat($productData['product_min_selling_price'], true, true).'</small>';
         }
 
