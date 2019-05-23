4ef2e419bc3e08a677263abe5250735c3952a0d4
Pawan Kumar dz <pawan.kumar.dz@ablysoft.com>
2019-05-23 17:39:09 +0530

    pushing changes

--- application/views/blog/test-view.php
+++ application/views/blog/test-view.php
@@ -1,5 +1,16 @@
 <section class="section bg--second">
 	<div class="container">
-		<div class="first-fold"></div>
+		<div class="first-fold">
+			<div class="post">
+				<figure class="post_media"><img src="<?php echo CONF_WEBROOT_URL;?>images/blog-main.jpg"></figure>
+				<div class="post_bg">
+					<ul class="post_category">
+						<li><a href="#">Home & Living</a></li>
+						<li><a href="#">Inspiration</a></li>
+					</ul>
+					<h2 class="post_title">	<a href="#">How To Choose The Most Profitable Dropshipping Products To Sell Online</a></h2>
+				</div>
+			</div>
+		</div>
 	</div>
 </section>
