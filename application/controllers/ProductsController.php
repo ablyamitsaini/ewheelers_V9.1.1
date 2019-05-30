914e5adaea65918522e84ff45b48eabb08e8c1fb
manpreet kaur <manpreet.kaur@fatbit.in>
2019-05-30 09:59:32 +0530

    task-63018-fixed upsell products related issue

--- application/controllers/ProductsController.php
+++ application/controllers/ProductsController.php
@@ -683,13 +683,14 @@ class ProductsController extends MyAppController
         if (1 > $selprod_id) {
             return;
         }
-
+        
+        $recentProductsArr = array();
         if (!isset($_COOKIE['recentViewedProducts'])) {
             setcookie('recentViewedProducts', $selprod_id.'_', time()+60*60*72, CONF_WEBROOT_URL);
+        } else {
+            $recentProducts = $_COOKIE['recentViewedProducts'];
+            $recentProductsArr = explode('_', $recentProducts);
         }
-
-        $recentProducts = $_COOKIE['recentViewedProducts'];
-        $recentProductsArr = explode('_', $recentProducts);
 
         $products = array();
 
