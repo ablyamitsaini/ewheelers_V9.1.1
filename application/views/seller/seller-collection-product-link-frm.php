3f3bbe4772360f597de71d21722d343054ef4a7a
manpreet kaur <manpreet.kaur@fatbit.in>
2019-06-10 18:47:11 +0530

    bug-027393-027385-027389-fixed some assigned bugs

--- application/views/seller/seller-collection-product-link-frm.php
+++ application/views/seller/seller-collection-product-link-frm.php
@@ -1,35 +1,47 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.');?>
-<div class="col-md-12">
-    <div class="">
-        <div class="tabs tabs-sm tabs--scroll clearfix">
-            <ul>
-                <li ><a onclick="getShopCollectionGeneralForm(<?php echo $scollection_id; ?>);" href="javascript:void(0)"><?php echo Labels::getLabel('TXT_Basic', $siteLangId);?></a></li>
-                <?php
-                foreach ($language as $lang_id => $langName) {?>
-                <li class=""><a href="javascript:void(0)" onClick="editShopCollectionLangForm(<?php echo $scollection_id ?>, <?php echo $lang_id;?>)">
-                    <?php echo $langName;?></a></li>
-                <?php } ?>
-                <li class="is-active">
-                    <a onclick="sellerCollectionProducts(<?php echo $scollection_id ?>)" href="javascript:void(0);"> <?php echo Labels::getLabel('TXT_LINK', $siteLangId);?> </a>
-                </li>
-                <li class=""><a
-                <?php if ($scollection_id > 0) {?>
-                    onclick="collectionMediaForm(this, <?php echo $scollection_id; ?>);"
-                <?php } ?> href="javascript:void(0);"><?php echo Labels::getLabel('TXT_Media', $siteLangId);?></a></li>
-            </ul>
+<div class="col-lg-10 col-md-10 mb-3">
+    <div class="content-header justify-content-between row mb-4">
+        <div class="content-header-left col-md-auto"><h5 class="cards-title"><?php echo Labels::getLabel('LBL_Shop_Collections', $siteLangId); ?></h5></div>
+        <div class="content-header-right col-auto">
+            <div class="form__group">
+                <a href="javascript:void(0)" onClick="shopCollections(this)" class="btn btn--primary-border"><?php echo Labels::getLabel('LBL_Back_to_Collections', $siteLangId);?></a>
+            </div>
         </div>
     </div>
+</div>
+<div class="col-lg-10 col-md-10">
+    <div class="col-md-12">
+        <div class="">
+            <div class="tabs tabs-sm tabs--scroll clearfix">
+                <ul>
+                    <li ><a onclick="getShopCollectionGeneralForm(<?php echo $scollection_id; ?>);" href="javascript:void(0)"><?php echo Labels::getLabel('TXT_Basic', $siteLangId);?></a></li>
+                    <?php
+                    foreach ($language as $lang_id => $langName) {?>
+                    <li class=""><a href="javascript:void(0)" onClick="editShopCollectionLangForm(<?php echo $scollection_id ?>, <?php echo $lang_id;?>)">
+                        <?php echo $langName;?></a></li>
+                    <?php } ?>
+                    <li class="is-active">
+                        <a onclick="sellerCollectionProducts(<?php echo $scollection_id ?>)" href="javascript:void(0);"> <?php echo Labels::getLabel('TXT_LINK', $siteLangId);?> </a>
+                    </li>
+                    <li class=""><a
+                    <?php if ($scollection_id > 0) {?>
+                        onclick="collectionMediaForm(this, <?php echo $scollection_id; ?>);"
+                    <?php } ?> href="javascript:void(0);"><?php echo Labels::getLabel('TXT_Media', $siteLangId);?></a></li>
+                </ul>
+            </div>
+        </div>
 
-    <div class="form__subcontent">
-        <div class="col-lg-6 col-md-6">
-            <?php
-            $sellerCollectionproductLinkFrm->setFormTagAttribute('onsubmit', 'setUpSellerCollectionProductLinks(this); return(false);');
-            $sellerCollectionproductLinkFrm->setFormTagAttribute('class', 'form form--horizontal');
-            $sellerCollectionproductLinkFrm->developerTags['colClassPrefix'] = 'col-lg-12 col-md- ';
-            $sellerCollectionproductLinkFrm->developerTags['fld_default_col'] = 12;
+        <div class="form__subcontent">
+            <div class="col-lg-6 col-md-6">
+                <?php
+                $sellerCollectionproductLinkFrm->setFormTagAttribute('onsubmit', 'setUpSellerCollectionProductLinks(this); return(false);');
+                $sellerCollectionproductLinkFrm->setFormTagAttribute('class', 'form form--horizontal');
+                $sellerCollectionproductLinkFrm->developerTags['colClassPrefix'] = 'col-lg-12 col-md- ';
+                $sellerCollectionproductLinkFrm->developerTags['fld_default_col'] = 12;
 
-            echo $sellerCollectionproductLinkFrm->getFormHtml(); ?>
-    </div>
+                echo $sellerCollectionproductLinkFrm->getFormHtml(); ?>
+        </div>
+        </div>
     </div>
 </div>
 
--- application/views/seller/shop-collections.php
+++ application/views/seller/shop-collections.php
@@ -2,22 +2,5 @@
 <?php $variables= array('language'=>$language,'siteLangId'=>$siteLangId,'shop_id'=>$shop_id,'action'=>$action);
 $this->includeTemplate('seller/_partial/shop-navigation.php', $variables, false); ?>
 <div class="tabs__content">
-    <div class="form__content row">
-        <div class="col-lg-10 col-md-10 mb-3">
-            <div class="content-header justify-content-between row mb-4">
-                <div class="content-header-left col-md-auto"><h5 class="cards-title"><?php echo Labels::getLabel('LBL_Shop_Collections', $siteLangId); ?></h5></div>
-                <div class="content-header-right col-auto">
-                    <div class="form__group">
-                        <a href="javascript:void(0)" onClick="toggleBulkCollectionStatues(1)" class="btn btn--primary formActionBtn-js formActions-css"><?php echo Labels::getLabel('LBL_Make_Active', $siteLangId);?></a>
-                        <a href="javascript:void(0)" onClick="toggleBulkCollectionStatues(0)" class="btn btn--primary-border formActionBtn-js formActions-css"><?php echo Labels::getLabel('LBL_Make_InActive', $siteLangId);?></a>
-                        <a href="javascript:void(0)" onClick="deleteSelectedCollection()" class="btn btn--primary formActionBtn-js formActions-css"><?php echo Labels::getLabel('LBL_Delete_selected', $siteLangId);?></a>
-                        <a href="javascript:void(0)" onClick="getShopCollectionGeneralForm(0)" class="btn btn--primary-border"><?php echo Labels::getLabel('LBL_Add_Collection', $siteLangId);?></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-10 col-md-10">
-            <div class="" id="shopFormChildBlock"></div>
-        </div>
-    </div>
+    <div class="form__content row" id="shopFormChildBlock"></div>
 </div>
