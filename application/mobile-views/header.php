0cfdf834973b1ab3fbc7027e6afc076c7248eee0
Satbir <satbir.kaushik@fatbit.in>
2019-06-11 13:49:04 +0530

    mobile_API_view_files

---
+++ application/mobile-views/header.php
@@ -0,0 +1,8 @@
+<?php defined('SYSTEM_INIT') or die('Invalid Usage.');
+$commonData = array(
+    'currencySymbol'=>$currencySymbol,
+    'totalFavouriteItems'=>$totalFavouriteItems,
+    'totalUnreadMessageCount'=>$totalUnreadMessageCount,
+    'totalUnreadNotificationCount'=>$totalUnreadNotificationCount,
+    'cartItemsCount'=>$cartItemsCount
+);
