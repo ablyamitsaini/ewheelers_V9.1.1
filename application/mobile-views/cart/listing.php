bb333fc9a8ad5a91c50abb351ec769da17d13a15
Satbir <satbir.kaushik@fatbit.in>
2019-07-05 16:48:06 +0530

    task-64235-cart listing API product image added

--- application/mobile-views/cart/listing.php
+++ application/mobile-views/cart/listing.php
@@ -5,6 +5,10 @@ $statusArr = array(
     'msg' => Labels::getLabel('MSG_Success', $siteLangId)
 );
 
+foreach ($products as $key => $product) {
+    $products[$key]['product_image_url'] = CommonHelper::generateFullUrl('image', 'product', array($product['product_id'], "CLAYOUT3", $product['selprod_id'], 0, $siteLangId));
+}
+
 $data = array(
     'products' => array_values($products),
     'cartSummary' => $cartSummary,
