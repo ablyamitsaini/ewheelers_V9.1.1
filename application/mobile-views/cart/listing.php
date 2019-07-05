6762f94dcdd38c3f5665ff3c9d6521891593b2b9
Satbir <satbir.kaushik@fatbit.in>
2019-07-05 16:27:48 +0530

    task-64235-Cart listing API response updated

--- application/mobile-views/cart/listing.php
+++ application/mobile-views/cart/listing.php
@@ -16,6 +16,32 @@ $data = array(
     'selectedShippingAddressId' => $selectedShippingAddressId,
 );
 
+$data['priceDetail'] = array(
+    array(
+        'key' => Labels::getLabel('LBL_Items', $siteLangId),
+        'value' => count($products)
+    ),
+    array(
+        'key' => Labels::getLabel('LBL_Total', $siteLangId),
+        'value' => CommonHelper::displayMoneyFormat($cartSummary['cartTotal'])
+    ),
+    array(
+        'key' => Labels::getLabel('LBL_Tax', $siteLangId),
+        'value' => CommonHelper::displayMoneyFormat($cartSummary['cartTaxTotal'])
+    )
+);
+
+if (isset($cartSummary['cartDiscounts']['coupon_discount_total'])) {
+    $data['priceDetail'][] = array(
+        'key' => Labels::getLabel('LBL_Discount', $siteLangId),
+        'value' => CommonHelper::displayMoneyFormat($cartSummary['cartDiscounts']['coupon_discount_total'])
+    );
+}
+$data['priceDetail'][] = array(
+    'key' => Labels::getLabel('LBL_Net_Payable', $siteLangId),
+    'value' => CommonHelper::displayMoneyFormat($cartSummary['orderNetAmount'])
+);
+
 if (1 > count($products)) {
     $statusArr['status'] = 0;
     $statusArr['msg'] = Labels::getLabel('MSG_No_record_found', $siteLangId);
