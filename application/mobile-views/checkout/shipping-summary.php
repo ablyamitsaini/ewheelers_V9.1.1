b598d34f7425dcac6defc48fbde5250e1bfc0a1e
Satbir <satbir.kaushik@fatbit.in>
2019-07-09 12:59:20 +0530

    task-64235-Shipping Summary API response updated

--- application/mobile-views/checkout/shipping-summary.php
+++ application/mobile-views/checkout/shipping-summary.php
@@ -22,7 +22,7 @@ $data = array(
 );
 
 
-if (1 > count($productSelectedShippingMethodsArr)) {
+if (1 > count($products)) {
     $statusArr['status'] = 0;
     $statusArr['msg'] = Labels::getLabel('MSG_No_record_found', $siteLangId);
 }
