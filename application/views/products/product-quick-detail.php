75b9e059cbea61e0a33c67082f4119b66e54eeae
manpreet kaur <manpreet.kaur@fatbit.in>
2019-07-11 15:41:28 +0530

    updated quick view HTML

--- application/views/products/product-quick-detail.php
+++ application/views/products/product-quick-detail.php
@@ -24,81 +24,83 @@
 
     <div class="col-lg-6 col-md-6 quick-col-2">
         <div class="product-detail product-description product-detail-quickview">
-            <div class="product-description-inner">
-                <div class="products__title"><a title="<?php echo $product['selprod_title']; ?>" href="<?php echo !isset($product['promotion_id']) ? CommonHelper::generateUrl('Products', 'View', array($product['selprod_id'])) : CommonHelper::generateUrl('Products', 'track', array($product['promotion_record_id']))?>"><?php echo $product['selprod_title'];?></a>
-                </div>
-                <div class="gap"></div>
-                <div class="products__price"><?php echo CommonHelper::displayMoneyFormat($product['theprice']); ?>
-                <?php if ($product['special_price_found']) { ?>
-                    <span class="products__price_old"><?php echo CommonHelper::displayMoneyFormat($product['selprod_price']); ?></span> <span class="product_off"><?php echo CommonHelper::showProductDiscountedText($product, $siteLangId); ?></span>
-                <?php } ?>
-                </div>
-                <div class="divider"></div>
-                <div class="gap"></div>
-            </div>
-
-            <?php if (!empty($optionRows)) { ?>
-            <div class="row">
-                <?php $selectedOptionsArr = $product['selectedOptionValues'];
-                $count = 0;
-                foreach ($optionRows as $key => $option) {
-                    $selectedOptionValue = $option['values'][$selectedOptionsArr[$key]]['optionvalue_name'];
-                    $selectedOptionColor = $option['values'][$selectedOptionsArr[$key]]['optionvalue_color_code']; ?>
-                <div class="col-md-6 mb-2">
-                    <div class="h6"><?php echo $option['option_name']; ?></div>
-                    <div class="js-wrap-drop-quick wrap-drop" id="js-wrap-drop-quick<?php echo $count; ?>">
-                        <span>
-                        <?php if ($option['option_is_color']) { ?>
-                            <span class="colors" style="background-color:#<?php echo $selectedOptionColor; ?>; ?>;"></span>
-                        <?php } ?>
-                        <?php echo $selectedOptionValue; ?></span>
-                        <?php if ($option['values']) { ?>
-                        <ul class="drop">
-                            <?php foreach ($option['values'] as $opVal) {
-                                $isAvailable = true;
-                                if (in_array($opVal['optionvalue_id'], $product['selectedOptionValues'])) {
-                                    $optionUrl = CommonHelper::generateUrl('Products', 'view', array($product['selprod_id']));
-                                    $selprodId = $product['selprod_id'];
-                                } else {
-                                    $optionUrl = Product::generateProductOptionsUrl($product['selprod_id'], $selectedOptionsArr, $option['option_id'], $opVal['optionvalue_id'], $product['product_id']);
-                                    $selprodId = Product::generateProductOptionsUrl($product['selprod_id'], $selectedOptionsArr, $option['option_id'], $opVal['optionvalue_id'], $product['product_id'], true);
-                                    $optionUrlArr = explode("::", $optionUrl);
-                                    if (is_array($optionUrlArr) && count($optionUrlArr) == 2) {
-                                        $optionUrl = $optionUrlArr[0];
-                                        $isAvailable = false;
-                                    }
-                                } ?>
-                                    <li class="<?php echo (in_array($opVal['optionvalue_id'], $product['selectedOptionValues'])) ? ' selected' : ' ';
-                                echo (!$optionUrl) ? ' is-disabled' : '';
-                                echo (!$isAvailable) ? 'not--available':''; ?>">
-                                        <?php if ($option['option_is_color'] && $opVal['optionvalue_color_code'] != '') { ?>
-                                        <a optionValueId="<?php echo $opVal['optionvalue_id']; ?>" selectedOptionValues="<?php echo implode("_", $selectedOptionsArr); ?>"
-                                            title="<?php echo $opVal['optionvalue_name'];
-                                            echo (!$isAvailable) ? ' '.Labels::getLabel('LBL_Not_Available', $siteLangId) : ''; ?>"
-                                            class="<?php echo (!$option['option_is_color']) ? 'selector__link' : '';
-                                            echo (in_array($opVal['optionvalue_id'], $product['selectedOptionValues'])) ? ' ' : ' '; echo (!$optionUrl) ? ' is-disabled' : '';  ?>"
-                                            href="javascript:void(0)" onclick="quickDetail(<?php echo $selprodId; ?>)"> <span class="colors" style="background-color:#<?php echo $opVal['optionvalue_color_code']; ?>;"></span><?php echo $opVal['optionvalue_name'];?></a>
-                                        <?php } else { ?>
-                                        <a optionValueId="<?php echo $opVal['optionvalue_id']; ?>" selectedOptionValues="<?php echo implode("_", $selectedOptionsArr); ?>"
-                                            title="<?php echo $opVal['optionvalue_name'];
-                                            echo (!$isAvailable) ? ' '.Labels::getLabel('LBL_Not_Available', $siteLangId) : ''; ?>"
-                                            class="<?php echo (in_array($opVal['optionvalue_id'], $product['selectedOptionValues'])) ? '' : ' '; echo (!$optionUrl) ? ' is-disabled' : '' ?>" href="javascript:void(0)" onclick="quickDetail(<?php echo $selprodId; ?>)">
-                                            <?php echo $opVal['optionvalue_name'];  ?> </a>
-                                        <?php } ?>
-                                    </li>
-                                    <?php
-                            } ?>
-
-                        </ul>
-                        <?php } ?>
+            <div>
+                <div class="product-description-inner">
+                    <div class="products__title"><a title="<?php echo $product['selprod_title']; ?>" href="<?php echo !isset($product['promotion_id']) ? CommonHelper::generateUrl('Products', 'View', array($product['selprod_id'])) : CommonHelper::generateUrl('Products', 'track', array($product['promotion_record_id']))?>"><?php echo $product['selprod_title'];?></a>
                     </div>
+                    <div class="gap"></div>
+                    <div class="products__price"><?php echo CommonHelper::displayMoneyFormat($product['theprice']); ?>
+                    <?php if ($product['special_price_found']) { ?>
+                        <span class="products__price_old"><?php echo CommonHelper::displayMoneyFormat($product['selprod_price']); ?></span> <span class="product_off"><?php echo CommonHelper::showProductDiscountedText($product, $siteLangId); ?></span>
+                    <?php } ?>
+                    </div>
+                    <div class="divider"></div>
+                    <div class="gap"></div>
                 </div>
-                    <?php $count++;
-                }?>
-            </div>
-            <?php } ?>
 
-            <div class="gap"></div>
+                <?php if (!empty($optionRows)) { ?>
+                <div class="row">
+                    <?php $selectedOptionsArr = $product['selectedOptionValues'];
+                    $count = 0;
+                    foreach ($optionRows as $key => $option) {
+                        $selectedOptionValue = $option['values'][$selectedOptionsArr[$key]]['optionvalue_name'];
+                        $selectedOptionColor = $option['values'][$selectedOptionsArr[$key]]['optionvalue_color_code']; ?>
+                    <div class="col-md-6 mb-2">
+                        <div class="h6"><?php echo $option['option_name']; ?></div>
+                        <div class="js-wrap-drop-quick wrap-drop" id="js-wrap-drop-quick<?php echo $count; ?>">
+                            <span>
+                            <?php if ($option['option_is_color']) { ?>
+                                <span class="colors" style="background-color:#<?php echo $selectedOptionColor; ?>; ?>;"></span>
+                            <?php } ?>
+                            <?php echo $selectedOptionValue; ?></span>
+                            <?php if ($option['values']) { ?>
+                            <ul class="drop">
+                                <?php foreach ($option['values'] as $opVal) {
+                                    $isAvailable = true;
+                                    if (in_array($opVal['optionvalue_id'], $product['selectedOptionValues'])) {
+                                        $optionUrl = CommonHelper::generateUrl('Products', 'view', array($product['selprod_id']));
+                                        $selprodId = $product['selprod_id'];
+                                    } else {
+                                        $optionUrl = Product::generateProductOptionsUrl($product['selprod_id'], $selectedOptionsArr, $option['option_id'], $opVal['optionvalue_id'], $product['product_id']);
+                                        $selprodId = Product::generateProductOptionsUrl($product['selprod_id'], $selectedOptionsArr, $option['option_id'], $opVal['optionvalue_id'], $product['product_id'], true);
+                                        $optionUrlArr = explode("::", $optionUrl);
+                                        if (is_array($optionUrlArr) && count($optionUrlArr) == 2) {
+                                            $optionUrl = $optionUrlArr[0];
+                                            $isAvailable = false;
+                                        }
+                                    } ?>
+                                        <li class="<?php echo (in_array($opVal['optionvalue_id'], $product['selectedOptionValues'])) ? ' selected' : ' ';
+                                    echo (!$optionUrl) ? ' is-disabled' : '';
+                                    echo (!$isAvailable) ? 'not--available':''; ?>">
+                                            <?php if ($option['option_is_color'] && $opVal['optionvalue_color_code'] != '') { ?>
+                                            <a optionValueId="<?php echo $opVal['optionvalue_id']; ?>" selectedOptionValues="<?php echo implode("_", $selectedOptionsArr); ?>"
+                                                title="<?php echo $opVal['optionvalue_name'];
+                                                echo (!$isAvailable) ? ' '.Labels::getLabel('LBL_Not_Available', $siteLangId) : ''; ?>"
+                                                class="<?php echo (!$option['option_is_color']) ? 'selector__link' : '';
+                                                echo (in_array($opVal['optionvalue_id'], $product['selectedOptionValues'])) ? ' ' : ' '; echo (!$optionUrl) ? ' is-disabled' : '';  ?>"
+                                                href="javascript:void(0)" onclick="quickDetail(<?php echo $selprodId; ?>)"> <span class="colors" style="background-color:#<?php echo $opVal['optionvalue_color_code']; ?>;"></span><?php echo $opVal['optionvalue_name'];?></a>
+                                            <?php } else { ?>
+                                            <a optionValueId="<?php echo $opVal['optionvalue_id']; ?>" selectedOptionValues="<?php echo implode("_", $selectedOptionsArr); ?>"
+                                                title="<?php echo $opVal['optionvalue_name'];
+                                                echo (!$isAvailable) ? ' '.Labels::getLabel('LBL_Not_Available', $siteLangId) : ''; ?>"
+                                                class="<?php echo (in_array($opVal['optionvalue_id'], $product['selectedOptionValues'])) ? '' : ' '; echo (!$optionUrl) ? ' is-disabled' : '' ?>" href="javascript:void(0)" onclick="quickDetail(<?php echo $selprodId; ?>)">
+                                                <?php echo $opVal['optionvalue_name'];  ?> </a>
+                                            <?php } ?>
+                                        </li>
+                                        <?php
+                                } ?>
+
+                            </ul>
+                            <?php } ?>
+                        </div>
+                    </div>
+                        <?php $count++;
+                    }?>
+                </div>
+                <?php } ?>
+
+                <div class="gap"></div>
+            </div>
             <!-- Add To Cart [ -->
             <?php if ($product['in_stock']) {
                 echo $frmBuyProduct->getFormTag();
