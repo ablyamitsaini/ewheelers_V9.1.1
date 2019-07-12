2250953946328a21c87a2febdbeda50291fd6203
Satbir <satbir.kaushik@fatbit.in>
2019-07-12 13:08:02 +0530

    task-64235-Shop view add product image

--- application/mobile-views/shops/view.php
+++ application/mobile-views/shops/view.php
@@ -5,6 +5,10 @@ $statusArr = array(
     'msg' => Labels::getLabel('MSG_Success', $siteLangId)
 );
 
+foreach ($data['products'] as $index => $product) {
+    $data['products'][$index]['product_image_url'] = CommonHelper::generateFullUrl('image', 'product', array($product['product_id'], "CLAYOUT3", $product['selprod_id'], 0, $siteLangId));
+}
+
 $data['shop'] = !empty($data['shop']) ? $data['shop'] : (object)array();
 
 if (1 > count((array)$data)) {
