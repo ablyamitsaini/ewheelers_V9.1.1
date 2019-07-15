4fdabd038f49ff9f452c5c1a1785069002635aac
anup <anup.rawat@ablysoft.com>
2019-07-15 17:28:23 +0530

    updated demo url check

--- application/utilities/CommonHelper.php
+++ application/utilities/CommonHelper.php
@@ -1860,7 +1860,7 @@ class CommonHelper extends FatUtility
 
     public static function demoUrl()
     {
-        if ($_SERVER['SERVER_NAME']== 'v8.demo.yo-kart.com') {
+        if (strpos($_SERVER ['SERVER_NAME'], 'v8.demo.yo-kart.com') !== false) {
             return true;
         }
         return false;
