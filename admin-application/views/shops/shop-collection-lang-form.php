3593913fa41c4c7c9bf732146a6d11c0d6675561
manpreet kaur <manpreet.kaur@fatbit.in>
2019-07-17 15:21:30 +0530

    task-65137-worked on shop collection navigation linking and volume discount issue

--- admin-application/views/shops/shop-collection-lang-form.php
+++ admin-application/views/shops/shop-collection-lang-form.php
@@ -1,7 +1,12 @@
-<?php defined('SYSTEM_INIT') or die('Invalid Usage.');?>
+<?php defined('SYSTEM_INIT') or die('Invalid Usage.');
+if (isset($scollection_id) && $scollection_id > 0) {
+    $scollection_id = $scollection_id;
+} else {
+    $scollection_id = 0;
+}?>
 <ul class="tabs_nav tabs_nav--internal">
     <li>
-        <a onclick="getShopCollectionGeneralForm(<?php echo $shop_id; ?>);" href="javascript:void(0)">
+        <a onclick="getShopCollectionGeneralForm(<?php echo $shop_id; ?>, <?php echo $scollection_id; ?>);" href="javascript:void(0)">
             <?php echo Labels::getLabel('TXT_GENERAL', $adminLangId);?>
         </a>
     </li>
--- admin-application/views/shops/shop-collection-media-form.php
+++ admin-application/views/shops/shop-collection-media-form.php
@@ -4,15 +4,14 @@
     $frm->developerTags['fld_default_col'] = 6;
     $fld = $frm->getField('collection_image');
     $fld->addFieldTagAttribute('class', 'btn btn--primary btn--sm');
-?>
-<ul class="tabs_nav tabs_nav--internal">
-    <li><a onclick="getShopCollectionGeneralForm(<?php echo $shop_id; ?>);" href="javascript:void(0)"><?php echo Labels::getLabel('TXT_GENERAL', $adminLangId);?></a></li>
-    <?php if (isset($scollection_id) && $scollection_id > 0) {
-        $scollection_id=$scollection_id;
+    if (isset($scollection_id) && $scollection_id > 0) {
+        $scollection_id = $scollection_id;
     } else {
-        $scollection_id='';
-    }
-    foreach ($language as $lang_id => $langName) {?>
+        $scollection_id = 0;
+    } ?>
+<ul class="tabs_nav tabs_nav--internal">
+    <li><a onclick="getShopCollectionGeneralForm(<?php echo $shop_id; ?>, <?php echo $scollection_id; ?>);" href="javascript:void(0)"><?php echo Labels::getLabel('TXT_GENERAL', $adminLangId);?></a></li>
+    <?php foreach ($language as $lang_id => $langName) { ?>
     <li><a href="javascript:void(0)" onClick="editShopCollectionLangForm(<?php echo $shop_id;?>, <?php echo $scollection_id ?>, <?php echo $lang_id;?>)">
         <?php echo Labels::getLabel('LBL_'.$langName, $adminLangId);?></a></li>
     <?php } ?>
