fb94e5064f5c240169f2f0b1e2013125b9fb5d23
Satbir <satbir.kaushik@fatbit.in>
2019-07-19 18:33:20 +0530

    task-64235-Image param added to offer listing API

--- application/mobile-views/buyer/search-offers.php
+++ application/mobile-views/buyer/search-offers.php
@@ -5,6 +5,9 @@ $statusArr = array(
     'msg' => Labels::getLabel('MSG_Success', $siteLangId)
 );
 
+foreach ($offers as $key => $offer) {
+    $offers[$key]['offerImage'] = CommonHelper::generateFullUrl('Image', 'coupon', array($offer['coupon_id'],$siteLangId,'NORMAL'));
+}
 $data = array(
     'offers'=> array_values($offers),
 );
