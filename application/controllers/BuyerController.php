61fdbb9f4e4b3155d77a0b1a68b9f423e93a6d99
Satbir <satbir.kaushik@fatbit.in>
2019-07-24 16:11:17 +0530

    task-64235-Update validation message if invalid user id passed

--- application/controllers/BuyerController.php
+++ application/controllers/BuyerController.php
@@ -2547,7 +2547,7 @@ class BuyerController extends BuyerBaseController
         $userObj = new User($userId);
         $userInfo = $userObj->getUserInfo(array('user_referral_code'), true, true);
         if (empty($userInfo['user_referral_code'])) {
-            FatUtility::dieJsonError(Labels::getLabel('Msg_Referral_Code_is_empty', $this->siteLangId));
+            FatUtility::dieJsonError(Labels::getLabel('MSG_Invalid_User', $this->siteLangId));
         }
 
         $referralTrackingUrl = CommonHelper::referralTrackingUrl($userInfo['user_referral_code']);
--- database/db-update.sql
+++ database/db-update.sql
@@ -377,3 +377,4 @@ DELETE FROM `tbl_language_labels` WHERE `label_key` = 'LBL_Please_add_minimum';
 ALTER TABLE `tbl_orders_status` ADD `orderstatus_color_code` VARCHAR(10) NULL DEFAULT NULL AFTER `orderstatus_identifier`;
 DELETE FROM `tbl_language_labels` WHERE `label_key` = 'L_Share_And_Earn_Text_Message';
 DELETE FROM `tbl_language_labels` WHERE `label_key` = 'LBL_Affiliate_Sharing_information_text';
+DELETE FROM `tbl_language_labels` WHERE `label_key` LIKE '%Msg_Referral_Code_is_empty%'
