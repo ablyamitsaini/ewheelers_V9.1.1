3964d450a7c2932dde212c857d26e3945664dd50
Satbir <satbir.kaushik@fatbit.in>
2019-07-25 10:25:45 +0530

    task-64235-Add default user image url on remove Image API

--- application/mobile-views/account/remove-profile-image.php
+++ application/mobile-views/account/remove-profile-image.php
@@ -5,4 +5,6 @@ $statusArr = array(
     'msg' => Labels::getLabel('MSG_Success', $siteLangId)
 );
 
-$data = array();
+$data = array(
+    'userImage' => CommonHelper::generateFullUrl('Account', 'userProfileImage', array(UserAuthentication::getLoggedUserId(), 'croped', true)).'?'.time()
+);
