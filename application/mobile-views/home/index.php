601f2b89ee78ac5dfd9d78e0d5c341273b334ed0
Satbir <satbir.kaushik@fatbit.in>
2019-07-31 10:49:45 +0530

    task-64235-Handle special characters in Home API

--- application/mobile-views/home/index.php
+++ application/mobile-views/home/index.php
@@ -12,20 +12,22 @@ foreach ($sponsoredShops as $shopIndex => $shopData) {
     }
 }
 foreach ($collections as $collectionIndex => $collectionData) {
-    if (isset($collectionData['products'])) {
+    if (array_key_exists('products', $collectionData)) {
         foreach ($collectionData['products'] as $index => $product) {
             $collections[$collectionIndex]['products'][$index]['product_image_url'] = CommonHelper::generateFullUrl('image', 'product', array($product['product_id'], "CLAYOUT3", $product['selprod_id'], 0, $siteLangId));
         }
-    } elseif (isset($collectionData['categories'])) {
+    } elseif (array_key_exists('categories', $collectionData)) {
         foreach ($collectionData['categories'] as $index => $category) {
+            $collections[$collectionIndex]['categories'][$index]['prodcat_name'] = html_entity_decode($category['prodcat_name'], ENT_QUOTES, 'utf-8');
+            $collections[$collectionIndex]['categories'][$index]['prodcat_description'] = strip_tags(html_entity_decode($category['prodcat_description'], ENT_QUOTES, 'utf-8'));
             $collections[$collectionIndex]['categories'][$index]['category_image_url'] = CommonHelper::generateFullUrl('Category', 'banner', array($category['prodcat_id'] , $siteLangId));
         }
-    } elseif (isset($collectionData['shops'])) {
+    } elseif (array_key_exists('shops', $collectionData)) {
         foreach ($collectionData['shops'] as $index => $shop) {
             $collections[$collectionIndex]['shops'][$index]['shop_logo'] = CommonHelper::generateFullUrl('image', 'shopLogo', array($shop['shop_id'], $siteLangId));
             $collections[$collectionIndex]['shops'][$index]['shop_banner'] = CommonHelper::generateFullUrl('image', 'shopBanner', array($shop['shop_id'], $siteLangId));
         }
-    } elseif (isset($collectionData['brands'])) {
+    } elseif (array_key_exists('brands', $collectionData)) {
         foreach ($collectionData['brands'] as $index => $shop) {
             $collections[$collectionIndex]['brands'][$index]['brand_image'] = CommonHelper::generateFullUrl('image', 'brand', array($shop['brand_id'], $siteLangId));
         }
