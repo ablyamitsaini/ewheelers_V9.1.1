817d5319ac91e8d01b8af5d05497fe3ef72b0731
Satbir <satbir.kaushik@fatbit.in>
2019-08-03 13:03:16 +0530

    bug-029024 - Default address is changed even when user clicks on cancel

--- application/views/account/page-js/my-addresses.js
+++ application/views/account/page-js/my-addresses.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-	searchAddresses();		
+	searchAddresses();
 });
 
 (function() {
 	var runningAjaxReq = false;
 	var dv = '#listing';
-	
+
 	checkRunningAjax = function(){
 		if( runningAjaxReq == true ){
 			console.log(runningAjaxMsg);
@@ -13,48 +13,49 @@ $(document).ready(function(){
 		}
 		runningAjaxReq = true;
 	};
-	
-	searchAddresses = function(){		
-		$(dv).html(fcom.getLoader());		
+
+	searchAddresses = function(){
+		$(dv).html(fcom.getLoader());
 		fcom.ajax(fcom.makeUrl('Account','searchAddresses'),'',function(res){
 			$(dv).html(res);
 		});
 	};
-	
+
 	addAddressForm = function(id){
 		$(dv).html(fcom.getLoader());
 		fcom.ajax(fcom.makeUrl('Account', 'addAddressForm', [id]), '', function(t) {
 			$(dv).html(t);
 		});
 	};
-	
+
 	setupAddress = function(frm){
-		if (!$(frm).validate()) return;		
+		if (!$(frm).validate()) return;
 		var data = fcom.frmData(frm);
-		fcom.updateWithAjax(fcom.makeUrl('Addresses', 'setUpAddress'), data, function(t) {						
+		fcom.updateWithAjax(fcom.makeUrl('Addresses', 'setUpAddress'), data, function(t) {
 			searchAddresses();
 		});
 	};
-	
-	setDefaultAddress = function(id){
+
+	setDefaultAddress = function(id, e){
 		if( !confirm(langLbl.confirmDefault) ){
+			e.preventDefault();
 			return false;
 		}
 		data='id='+id;
-		fcom.updateWithAjax(fcom.makeUrl('Addresses','setDefault'),data,function(res){		
+		fcom.updateWithAjax(fcom.makeUrl('Addresses','setDefault'),data,function(res){
 			searchAddresses();
 		});
 	};
-	
+
 	removeAddress = function(id){
 		var agree = confirm(langLbl.confirmDelete);
 		if( !agree ){
 			return false;
 		}
 		data='id='+id;
-		fcom.updateWithAjax(fcom.makeUrl('Addresses','deleteRecord'),data,function(res){		
+		fcom.updateWithAjax(fcom.makeUrl('Addresses','deleteRecord'),data,function(res){
 			searchAddresses();
 		});
 	};
-	
-})();	
+
+})();
--- application/views/account/search-addresses.php
+++ application/views/account/search-addresses.php
@@ -16,7 +16,7 @@
         $address['ua_identifier'] = ($address['ua_identifier'] == '') ? '&nbsp;' : $address['ua_identifier']; ?> <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
             <label class="list__selection <?php echo ($address['ua_is_default']==1)?'is-active':''; ?>">
                 <span class="radio">
-                    <input type="radio" <?php echo ($address['ua_is_default']==1)?'checked=""':''; ?> name="1" onClick="setDefaultAddress(<?php echo $address['ua_id']; ?>)"><i class="input-helper"></i>
+                    <input type="radio" <?php echo ($address['ua_is_default']==1)?'checked=""':''; ?> name="1" onClick="setDefaultAddress(<?php echo $address['ua_id']; ?>, event)"><i class="input-helper"></i>
                 </span>
                 <address>
                     <h6><?php echo $address['ua_identifier']; ?></h6>
