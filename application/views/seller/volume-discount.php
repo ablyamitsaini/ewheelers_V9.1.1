56698366af8905aff36ebda10242b2f883618ea5
Pawan Kumar dz <pawan.kumar.dz@ablysoft.com>
2019-08-23 14:01:23 +0530

    task-66013-designed volume discount page

--- application/views/seller/volume-discount.php
+++ application/views/seller/volume-discount.php
@@ -1,5 +1,5 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.');
-    $frmSearch->setFormTagAttribute('class', 'web_form last_td_nowrap');
+    $frmSearch->setFormTagAttribute('class', 'form');
     $frmSearch->setFormTagAttribute('onsubmit', 'searchVolumeDiscountProducts(this); return(false);');
     $frmSearch->developerTags['colClassPrefix'] = 'col-md-';
     $frmSearch->developerTags['fld_default_col'] = 4;
@@ -19,7 +19,7 @@
     $cancelBtnFld->developerTags['col'] = 2;
     $cancelBtnFld->developerTags['noCaptionTag'] = true;
 
-    $addVolDiscountfrm->setFormTagAttribute('class', 'web_form last_td_nowrap');
+    $addVolDiscountfrm->setFormTagAttribute('class', 'form');
     $addVolDiscountfrm->setFormTagAttribute('id', 'frmAddVolumeDiscount');
     $addVolDiscountfrm->setFormTagAttribute('onsubmit', 'updateVolumeDiscount(this); return(false);');
 
@@ -45,18 +45,13 @@
                 <?php $this->includeTemplate('_partial/dashboardTop.php'); ?>
                 <h2 class="content-header-title"><?php echo Labels::getLabel('LBL_Manage_Volume_Discount', $siteLangId); ?></h2>
             </div>
-            <div class="col-auto">
-                <div class="action">
-                    <a class="btn btn--primary btn--sm formActionBtn-js formActions-css" title="<?php echo Labels::getLabel('LBL_Remove_Volume_Discount', $siteLangId); ?>" onclick="deleteVolumeDiscount()" href="javascript:void(0)"><?php echo Labels::getLabel('LBL_Remove_Volume_Discount', $siteLangId); ?></a>
-                    <div class="gap"></div>
-                </div>
-            </div>
+            
         </div>
         <div class="content-body">
             <div class="row mb-4">
                 <div class="col-lg-12">
                     <div class="cards">
-                        <div class="cards-content pt-4 pl-4 pr-4 pb-4">
+                        <div class="cards-content pt-4 pl-4 pr-4 pb-0">
                             <div class="replaced">
                                 <?php echo $frmSearch->getFormHtml(); ?>
                             </div>
@@ -67,7 +62,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="cards">
-                        <div class="cards-content pl-4 pr-4 pt-4">
+                        <div class="cards-content pt-4 pl-4 pr-4 pb-0">
                             <div class="replaced">
                                 <?php
                                     echo $addVolDiscountfrm->getFormTag();
@@ -108,13 +103,24 @@
                                     </div>
                                 </form>
                             </div>
-                            <span class="gap"></span>
-                            <div class="divider"></div>
+                        </div>
+                         <div class="divider"></div>
+					<div class="cards-content pl-4 pr-4">
+						
+                            <div class="row justify-content-between">
+								<div class="col-auto"></div>
+                            	 <div class="col-auto">
+                <div class="action">
+                    <a class="btn btn--primary-border btn--sm formActionBtn-js formActions-css" title="<?php echo Labels::getLabel('LBL_Remove_Volume_Discount', $siteLangId); ?>" onclick="deleteVolumeDiscount()" href="javascript:void(0)"><?php echo Labels::getLabel('LBL_Remove_Volume_Discount', $siteLangId); ?></a>
+                    
+                </div>
+            </div>                           	
+                            </div>
                             <div id="listing">
                                 <?php echo Labels::getLabel('LBL_Loading..', $siteLangId); ?>
                             </div>
                             <span class="gap"></span>
-                        </div>
+					</div>
                     </div>
                 </div>
             </div>
