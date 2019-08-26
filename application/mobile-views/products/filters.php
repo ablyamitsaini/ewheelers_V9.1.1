d085217e2f86e43a08f90ff7ae5113fb3bc7afe3
Satbir <satbir.kaushik@fatbit.in>
2019-08-26 13:11:35 +0530

    task-64235-Product filters conditions param and brand param accepted format

--- application/mobile-views/products/filters.php
+++ application/mobile-views/products/filters.php
@@ -1,5 +1,17 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.');
 
+$conditions = array();
+$conditionTitles = Product::getConditionArr($siteLangId);
+foreach ($conditionsArr as $condition) {
+    if ($condition['selprod_condition'] == 0) {
+        continue;
+    }
+    $conditions[] = array(
+        'title' => $conditionTitles[$condition['selprod_condition']],
+        'value' => $condition['selprod_condition'],
+    );
+}
+
 $data = array(
     'productFiltersArr' => empty($productFiltersArr) ? (object)array() : $productFiltersArr,
     'headerFormParamsAssocArr' => $headerFormParamsAssocArr,
@@ -9,7 +21,7 @@ $data = array(
     'brandsArr' => $brandsArr,
     'brandsCheckedArr' => $brandsCheckedArr,
     'optionValueCheckedArr' => $optionValueCheckedArr,
-    'conditionsArr' => $conditionsArr,
+    'conditionsArr' => $conditions,
     'conditionsCheckedArr' => $conditionsCheckedArr,
     'options' => $options,
     'priceArr' => $priceArr,
