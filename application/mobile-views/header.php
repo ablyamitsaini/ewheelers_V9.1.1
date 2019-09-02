d201e7145216b0629cc1cdb40be2678f4d4f8ae3
Satbir <satbir.kaushik@fatbit.in>
2019-09-02 17:24:42 +0530

    task-66013-Remove unwanted files

--- application/mobile-views/header.php
+++
@@ -1,8 +0,0 @@
-<?php defined('SYSTEM_INIT') or die('Invalid Usage.');
-$commonData = array(
-    'currencySymbol'=>$currencySymbol,
-    'totalFavouriteItems'=>$totalFavouriteItems,
-    'totalUnreadMessageCount'=>$totalUnreadMessageCount,
-    'totalUnreadNotificationCount'=>$totalUnreadNotificationCount,
-    'cartItemsCount'=>$cartItemsCount
-);
