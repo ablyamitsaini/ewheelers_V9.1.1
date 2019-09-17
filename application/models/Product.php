41c20179daa10dd0870556399c77512d17d7f41a
Satbir <satbir.kaushik@fatbit.in>
2019-09-17 15:14:22 +0530

    task-64235-Set condition for option value in get filtered products API

--- application/models/Product.php
+++ application/models/Product.php
@@ -1192,6 +1192,10 @@ class Product extends MyAppModel
             $keyword = $criteria['keyword'];
         }
 
+        if (true ===  MOBILE_APP_API_CALL) {
+            $criteria['optionvalue'] = !empty($criteria['optionvalue']) ? json_decode($criteria['optionvalue'], true) : '';
+        }
+
         $srch->setDefinedCriteria($join_price, 0, $criteria, true);
         $srch->joinProductToCategory();
         $srch->joinSellerSubscription(0, false, true);
