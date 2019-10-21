203cc00286fa263f7e0e7da603ae38623f324c18
Pawan Kumar dz <pawan.kumar.dz@ablysoft.com>
2019-10-21 14:58:44 +0530

    task-67762 fixed sticky demo header

--- application/views/restore-system/top-header.php
+++ application/views/restore-system/top-header.php
@@ -1,7 +1,11 @@
 <div class="demo-header no-print">
     <div class="restore-wrapper">
         <a href="javascript:void(0)" onclick="showRestorePopup()">
-		<p class="restore__content">Database Will Restore in</p>
+        
+		<p class="restore__content">Database Restore in</p>
+           <div class="restore__progress">
+<div class="restore__progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
             <span class="restore__counter" id="restoreCounter">00:00:00</span>                        
         </a>
     </div>
@@ -23,8 +27,8 @@
                 </i></a></li> <?php  */?>
     </ul>    
     <div class="demo-cta">
-        <a target="_blank" href="https://www.yo-kart.com/multivendor-ecommerce-marketplace-platform.html" class="btn btn--primary-border ripplelink btn--sm">Start Your Marketplace</a> &nbsp;		
-		<a href="javascript:void(0);" class="request-demo btn btn--secondary ripplelink btn--sm" id="btn-demo" >Request a Demo</a>
-        <!-- <a href="javascript:void(0)" class="close-layer"></a> -->
+        <a target="_blank" href="https://www.yo-kart.com/multivendor-ecommerce-marketplace-platform.html" class=" btn btn--primary ripplelink">Start Your Marketplace</a> &nbsp;		
+		<a href="javascript:void(0);" class="request-demo btn btn--primary-border ripplelink" id="btn-demo" >Request a Demo</a>
+       <a href="javascript:void(0)" class="close-layer"></a>
     </div>	
 </div>
